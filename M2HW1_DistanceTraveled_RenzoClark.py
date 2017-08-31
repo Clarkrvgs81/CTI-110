7f2e9b743a2fdbb3253f217b173a96b0b8ade226
Clarkrvgs81 <31522849+Clarkrvgs81@users.noreply.github.com>
2017-08-31 19:15:17 -0400

    Add files via upload
    Second submission

---
+++ M2HW1_DistanceTraveled_RenzoClark.py
@@ -0,0 +1,10 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# Renzo Clark
+# August 31, 2017
+
+speed = 70
+print( "In 6 hours, the car travels " + str( ( speed * 6 ) ) + " miles " )
+print( "In 10 hours, the car travels " + str( ( speed * 10 ) ) + " miles " )
+print( "In 15 hours, the car travels " + str( ( speed * 15 ) ) + " miles " )
+
