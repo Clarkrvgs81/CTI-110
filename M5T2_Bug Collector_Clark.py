a359e437be11cfcd37d8312a0276ba3c080aa072
Clarkrvgs81 <31522849+Clarkrvgs81@users.noreply.github.com>
2017-10-31 17:36:02 -0400

    Add files via upload

---
+++ M5T2_Bug Collector_Clark.py
@@ -0,0 +1,14 @@
+# CTI-110 
+# M5T2 - Bug Collector  
+# Renzo Clark
+# Oct 19, 2017
+
+
+total = 0
+for day in range(1, 8):
+    print('Enter the bugs collected on day', day)
+    bugs = int(input())
+    total += bugs
+print('You collected a total of', total, 'bugs.')
+
+    
